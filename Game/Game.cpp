7a58acc281df34c902ae52d0646db7ee766d5565
世合 王 <812690683@qq.com>
2021-08-25 15:38:54 +0800

    添加项目文件。

---
+++ Game/Game.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <SDL.h>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING))
+    {
+        SDL_Log("启动失败%s", SDL_GetError());
+    }
+    SDL_Window* win = SDL_CreateWindow(
+        "WINDOW", 0, 0, 500, 500, SDL_WINDOW_MAXIMIZED
+    );
+    SDL_Delay(3000);
+    return 0;
+}
